4221765d98a86bace13259be9a40d6330dfff96f
Jack Buckon <buckon99@live.com>
2017-10-28 14:43:52 -0700

    added logic for saving current order book

--- app.js
+++ app.js
@@ -1,23 +1,87 @@
 /*MAIN PROGRAM FOR CRYPTODATACOLLECTOR*/
-var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000,
-mongoose = require('mongoose'),
-bodyParser = require('body-parser');
+var WebSocket  = require('ws');
+const mongoose = require('mongoose');
+var ws = new WebSocket('wss://ws-feed.gdax.com');
+const OrderBookItem = require('./models/OrderBookItem');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/cryptodata'); 
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.');
+  process.exit();
+});
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-var routes = require('./api/routes/todoListRoutes');
-
-routes(app);
-
-
-app.listen(port);
-
-console.log('RESTful API server started on: ' + port);
+mongoose.connect('mongodb://localhost/cryptodata');
+ws.on('open', function() {
+    var data = {
+        "type": "subscribe",
+        "product_ids": [
+            "ETH-USD"
+        ],
+        "channels": [
+            /*"full",*/
+            "level2"
+        ]
+    };
+    ws.send(JSON.stringify(data));
+});
+ws.on('message', function(message) {
+    var json = JSON.parse(message);
+    if(json.type == "snapshot"){
+        OrderBookItem.remove({}, function(err){
+            if(err)
+                console.log("Error clearing all order book items: " + err);
+            json.bids.forEach(function(entry){
+                var item = new OrderBookItem({
+                    product_id: json.product_id,
+                    order_type: "buy",
+                    price: entry[0],
+                    amount: entry[1]
+                });
+                item.save((err) => {
+                    if (err) { return next(err); }
+                });
+            });
+            json.asks.forEach(function(entry){
+                var item = new OrderBookItem({
+                    product_id: json.product_id,
+                    order_type: "sell",
+                    price: entry[0],
+                    amount: entry[1]
+                });
+                item.save((err) => {
+                    if (err) { return next(err); }
+                });
+            });
+        });
+    }
+    else if(json.type == "l2update"){
+        json.changes.forEach(function(entry){
+            if(entry[2] == "0")
+            {
+                OrderBookItem.remove({product_id: json.product_id, order_type: entry[0], price: entry[1]}, function(err){
+                    if(err)
+                        console.log("error deleting order book item: " + err);
+                });
+            }else{
+                OrderBookItem.findOne({product_id: json.product_id, order_type: entry[0], price: entry[1]}, function(error, result) {
+                    if(result == null){
+                        var item = new OrderBookItem({
+                            product_id: json.product_id,
+                            order_type: entry[0],
+                            price: entry[1],
+                            amount: entry[2]
+                        });
+                        item.save((err) => {
+                            if (err) { return next(err); }
+                        });
+                    }else{
+                        result.amount = entry[2];
+                        result.save((err) => {
+                            if (err) { return next(err); }
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
